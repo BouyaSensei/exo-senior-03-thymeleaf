31eba82021452bf6074d9b76e1d6522c5edb2503
BouyaSensei <buildario@outlook.fr>
2026-07-01 19:10:47 +0200

    Ajout de la classe StatusTagProcessor pour le traitement des balises personnalisées dans Thymeleaf

---
+++ dialect/src/main/java/com/dialect/tags/StatusTagProcessor.java
@@ -0,0 +1,33 @@
+package com.dialect.tags;
+
+import org.thymeleaf.context.ITemplateContext;
+import org.thymeleaf.engine.AttributeName;
+import org.thymeleaf.model.IProcessableElementTag;
+import org.thymeleaf.processor.element.AbstractAttributeTagProcessor;
+import org.thymeleaf.processor.element.IElementTagStructureHandler;
+import org.thymeleaf.templatemode.TemplateMode;
+
+public class StatusTagProcessor extends AbstractAttributeTagProcessor {
+
+    private static final String ATTR_NAME = "badge";
+    private static final int PRECEDENCE = 10000; // Priorité haute
+
+    public StatusTagProcessor(String dialectPrefix) {
+        super(
+                TemplateMode.HTML,
+                dialectPrefix,
+                null,
+                false,
+                ATTR_NAME,
+                true,
+                PRECEDENCE,
+                true);
+    }
+
+    @Override
+    protected void doProcess(ITemplateContext context, IProcessableElementTag tag,
+            AttributeName attributeName, String attributeValue,
+            IElementTagStructureHandler structureHandler) {
+
+    }
+}
